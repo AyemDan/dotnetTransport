56c2c80d59bf133077e48598020c4ae72cfbf618
AyemDanm <danielMMomodu@gmail.com>
2025-06-18 18:25:29 +0100

    Base code

--- src/Transport.Application/obj/Debug/net9.0/Transport.Application.AssemblyInfo.cs
+++ src/Transport.Application/obj/Debug/net9.0/Transport.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Transport.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90c2d7f1060de6d71fb43ac4aa8df77662a8d786")]
 [assembly: System.Reflection.AssemblyProductAttribute("Transport.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Transport.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
